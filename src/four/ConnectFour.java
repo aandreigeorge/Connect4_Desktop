07da04ca4fb23254070754a5d841092dc2e722a5
Andrei Avram <andreiavram91@gmail.com>
2024-04-24 15:43:53 +0300

    Stage 3 Implementation

--- src/four/ConnectFour.java
+++ src/four/ConnectFour.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class ConnectFour extends JFrame {
 
     private boolean isNextMoveX = true;
+    private final JButton[][] cells = new JButton[6][7];
 
     public ConnectFour() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,18 +17,16 @@ public class ConnectFour extends JFrame {
         JPanel boardPanel = new JPanel(new GridLayout(6, 7));
 
         for (int row = 5; row >= 0; row--) {
-            for (int column = 0; column < 7; column++){
-                JButton cell = new JButton();
-                cell.setName("Button" + (char) ('A' + column) + (row + 1));
-                cell.setText(" ");
-                boardPanel.add(cell);
+            for (int column = 0; column < 7; column++) {
+                cells[row][column] = new JButton();
+                cells[row][column].setName("Button" + (char) ('A' + column) + (row + 1));
+                cells[row][column].setText(" ");
+                boardPanel.add(cells[row][column]);
 
-                cell.addActionListener(e -> {
+                cells[row][column].addActionListener(e -> {
                     JButton clickedCell = (JButton) e.getSource();
-                    if (clickedCell.getText().equals(" ")) {
-                        clickedCell.setText(isNextMoveX ? "X" : "O");
-                        isNextMoveX = !isNextMoveX;
-                    }
+                    int columnClicked = getColumnNumberFromButtonName(clickedCell.getName());
+                    placePiece(columnClicked);
                 });
             }
         }
@@ -35,6 +34,26 @@ public class ConnectFour extends JFrame {
         add(boardPanel);
         setVisible(true);
     }
+
+    private int getColumnNumberFromButtonName(String buttonName) {
+        return buttonName.charAt(6) - 'A';
+    }
+
+    private void placePiece(int column) {
+        int row = getFirstEmptyRow(column);
+        if (row != -1) {
+            cells[row][column].setText(isNextMoveX ? "X" : "O");
+            isNextMoveX = !isNextMoveX;
+        }
+    }
+
+    private int getFirstEmptyRow(int column) {
+        for (int row = 0; row <= 5; row++) {
+            if (cells[row][column].getText().equals(" ")) {
+                return row;
+            }
+        }
+        return -1;
+    }
+
 }
-
-
